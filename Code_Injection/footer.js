9125f0546e0baee1b227445b12cb6e8839ef1b50
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-07-07 07:01:28 +0300

    Update footer.js

--- Code_Injection/footer.js
+++ Code_Injection/footer.js
@@ -1,3 +1,4 @@
+// This remains the same as it's generic tracking code
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Page loaded successfully');
     if (typeof ga === 'function') {
